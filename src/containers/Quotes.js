d2a384f66e7e503e251b4db87367caba77e7abfa
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-01-12 09:56:00 -0800

    commit tree d16904f52e9037d2ed8f70f32113e9ab292290fd

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeQuote, downvoteQuote, upvoteQuote } from '../actions/quotes';
 import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
 
   render() {
+    const {quotes, removeQuote, upvoteQuote, downvoteQuote} = this.props
     return (
       <div>
         <hr />
@@ -20,6 +22,7 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+              {quotes.map(quote => <QuoteCard quote={quote} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote}/>)}
             </div>
           </div>
         </div>
@@ -28,5 +31,11 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes
+  }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, {upvoteQuote, downvoteQuote, removeQuote})(Quotes);
